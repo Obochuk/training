c56c53b7cd39d62b5e46d3d8837b06c068dee159
Obochuk <obochuckv@gmail.com>
2018-03-16 17:58:16 +0200

    new task

--- src/epam/training/practice3/task2/carriage/passangers/PassangersCarriage.java
+++ src/epam/training/practice3/task2/carriage/passangers/PassangersCarriage.java
@@ -3,6 +3,21 @@ package epam.training.practice3.task2.carriage.passangers;
 public abstract class PassangersCarriage {
     private int seats;
     private int disabledPeopleSeats;
+    private boolean bedding;
+
+    public PassangersCarriage(int seats) {
+        this(seats, 0);
+    }
+
+    public PassangersCarriage(int seats, int disabledPeopleSeats) {
+        this(seats, disabledPeopleSeats, false);
+    }
+
+    public PassangersCarriage(int seats, int disabledPeopleSeats, boolean bedding) {
+        this.seats = seats;
+        this.disabledPeopleSeats = disabledPeopleSeats;
+        this.bedding = bedding;
+    }
 
     public int getSeats() {
         return seats;
@@ -18,5 +33,13 @@ public abstract class PassangersCarriage {
 
     public void setDisabledPeopleSeats(int disabledPeopleSeats) {
         this.disabledPeopleSeats = disabledPeopleSeats;
+    }
+
+    public boolean isBedding() {
+        return bedding;
+    }
+
+    public void setBedding(boolean bedding) {
+        this.bedding = bedding;
     }
 }
