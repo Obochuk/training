f28f4424fb77fa3d71615ceb86806f857713f1ef
Obochuk <obochuckv@gmail.com>
2018-04-23 10:48:15 +0300

    services implementations added

--- src/epam/training/practice10/src/main/java/entity/worker/WorkerService.java
+++ src/epam/training/practice10/src/main/java/entity/worker/WorkerService.java
@@ -2,9 +2,50 @@ package entity.worker;
 
 
 import database.GenericService;
+import database.query.Queries;
+import entity.department.Department;
+import org.apache.log4j.Logger;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorkerService extends GenericService<Worker>{
+    private final static Logger LOGGER = Logger.getLogger(WorkerService.class);
+
     public WorkerService() {
         super(new WorkerDAO());
+    }
+
+    public List<Worker> getAll(){
+        try (PreparedStatement statement = connection.prepareStatement(Queries.GET_ALL_WORKERS)){
+
+            return dao.select(statement);
+        } catch (SQLException e){
+            LOGGER.error("Cannot prepare statement", e);
+        }
+        return new ArrayList<>();
+    }
+
+    public List<Worker> getWorkersOfDepartment(Department department){
+        try (PreparedStatement statement = connection.prepareStatement(Queries.GET_WORKERS_OF_DEPARTMENT)){
+
+            statement.setInt(1, department.getId());
+            return dao.select(statement);
+        } catch (SQLException e){
+            LOGGER.error("Cannot prepare statement", e);
+        }
+        return new ArrayList<>();
+    }
+
+    public void delete(Worker worker){
+        try (PreparedStatement statement = connection.prepareStatement(Queries.DELETE_WORKER)) {
+
+            statement.setInt(1, worker.getId());
+            dao.update(statement);
+        } catch (SQLException e){
+            LOGGER.error("Cannot prepare statement", e);
+        }
     }
 }
